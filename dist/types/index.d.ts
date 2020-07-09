e3ad3b391ab9dd622e10fc7c19053318d1d930d1
Barış Esen <barisesennn@gmail.com>
2020-07-09 13:09:54 +0300

    dist directory ignored

--- dist/types/index.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="react" />
-import { LazyLoadProps } from "./interface";
-declare function Lazy(props: LazyLoadProps): JSX.Element;
-export default Lazy;
--- dist/types/interface.d.ts
+++
@@ -1,7 +0,0 @@
-import { ImgHTMLAttributes } from "react";
-export interface LazyLoadProps extends Partial<ImgHTMLAttributes<any>> {
-    ratio: number;
-    placeholder: string;
-    force?: boolean;
-    onVisible?: Function;
-}
